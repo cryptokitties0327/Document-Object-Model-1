be49513449f03846b14baa5ae551bca8d3428704
cryptokitties0327 <kimberley_tse@yahoo.com.au>
2018-11-18 17:32:10 +1100

    second commit - events

--- dom.js
+++ dom.js
@@ -89,3 +89,128 @@
 //   even[i].style.backgroundColor = '#ccc';
 // }
 
+
+
+// TRAVERSING THE DOM //
+// var itemList = document.querySelector('#items');
+// parentNode
+// console.log(itemList.parentNode);
+// itemList.parentNode.style.backgroundColor = '#f4f4f4';
+// console.log(itemList.parentNode.parentNode.parentNode);
+
+// parentElement
+// console.log(itemList.parentElement);
+// itemList.parentElement.style.backgroundColor = '#f4f4f4';
+// console.log(itemList.parentElement.parentElement.parentElement);
+
+// childNodes
+// console.log(itemList.childNodes);
+
+// console.log(itemList.children);
+// console.log(itemList.children[1]);
+// itemList.children[1].style.backgroundColor = 'yellow';
+
+// // FirstChild
+// console.log(itemList.firstChild);
+// // firstElementChild
+// console.log(itemList.firstElementChild);
+// itemList.firstElementChild.textContent = 'Hello 1';
+
+
+// lastChild
+// console.log(itemList.lastChild);
+// lastElementChild
+// console.log(itemList.lastElementChild);
+// itemList.lastElementChild.textContent = 'Hello 4';
+
+// nextSibling
+// console.log(itemList.nextSibling);
+// // nextElementSibling
+// console.log(itemList.nextElementSibling);
+
+// previousSibling
+// console.log(itemList.previousSibling);
+// previousElementSibling
+// console.log(itemList.previousElementSibling);itemList.previousElementSibling.style.color = 'green';
+
+// createElement
+
+// Create a div
+// var newDiv =  document.createElement('div');
+
+// // Add class
+// newDiv.className= 'hello';
+
+// // Add id
+// newDiv.id = 'hello1';
+
+// // Add attr
+// newDiv.setAttribute('title', 'Hello Div');
+
+// // Create text node
+// var newDivText = document.createTextNode('Hello World');
+
+// // Add text to div
+// newDiv.appendChild(newDivText);
+
+// var container = document.querySelector('header .container');
+// var h1 = document.querySelector('header h1');
+
+// console.log(newDiv);
+
+// newDiv.style.fontSize = '30px';
+
+// container.insertBefore(newDiv, h1);
+
+// //--Events
+// var button = document.getElementById("button").addEventListener("click", buttonClick)
+
+// function buttonClick(e){
+//   // document.getElementById("header-title").textContent = "Changes";
+//   // document.querySelector('#main').style.background = "#f4f4f4"
+//   // console.log(e)
+//   // console.log(e.target)
+//   // console.log(e.target.id)
+//   // console.log(e.target.className)
+//   // console.log(e.target.classList)
+//   //var output = document.getElementById("output")
+//   // output.innerHTML = '<h3>'+e.target.id+'</h3>'
+//   // console.log(e.clientX)
+//   // console.log(e.clientY)
+//   // console.log(e.offsetX)
+//   // console.log(e.offsetY)
+//   console.log(e.altKey)
+//   console.log(e.ctrlKey)
+//   console.log(e.shiftKey)
+
+// }
+
+// var input = document.getElementById("input").addEventListener("input", outputBox)
+
+// function outputBox(e){
+//   var output = document.getElementById("output-box")
+//   console.log(output)
+//   console.log(document.getElementById("input").value)
+//   output.innerHTML = document.getElementById("input").value
+// }
+
+var button = document.getElementById("button");
+var box = document.getElementById('box');
+
+// button.addEventListener("click", runEvent);
+// button.addEventListener("dblclick", runEvent);
+// button.addEventListener("mousedown", runEvent);
+// button.addEventListener("mouseup", runEvent);
+
+// box.addEventListener('mouseenter', runEvent)
+// box.addEventListener('mouseleave', runEvent)
+// box.addEventListener('mouseover', runEvent)
+// box.addEventListener('mouseout', runEvent)
+box.addEventListener('mousemove', runEvent)
+
+
+function runEvent(e){
+  console.log('Event Type: '+e.type);
+  output.innerHTML = '<h3>MouseX:'+e.offsetX+'</h3>'
+}
+
